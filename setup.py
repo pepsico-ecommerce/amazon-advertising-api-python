1f46704413bf2fbf1dc3b8004aa019f1a40f44e6
dkoryukova <dkoryukova@provectus.com>
2020-10-15 18:22:59 +0300

    "Bump version: 1.0 → 0.0-dev"

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import amazon_advertising_api.versions as aa_versions
 setup(
     name='amazon_advertising_api',
     packages=['amazon_advertising_api'],
-    version=aa_versions.versions['application_version'],
+    version='0.0-dev',
     description='Unofficial Amazon Sponsored Products Python client library.',
     url='https://github.com/pepsico-ecommerce/amazon-advertising-api-python')
