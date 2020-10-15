8cdb22e9d3773f8c1ad7fd6923b0befcf0a23a4f
Argo CI <argo-ci@pepsi.co>
2020-10-15 16:44:05 +0000

    "Bump version: 0.0.2-alpha → 0.0.2"

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 setup(
     name='amazon_advertising_api',
     packages=['amazon_advertising_api'],
-    version='0.0.1',
+    version='0.0.2',
     description='Unofficial Amazon Sponsored Products Python client library.',
     url='https://github.com/pepsico-ecommerce/amazon-advertising-api-python')
