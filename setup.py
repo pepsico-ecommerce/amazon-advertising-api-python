c00d982e0a67089da37a0053c5c4a4d04e88f725
Daria Koryukova <33223711+dkoryukova@users.noreply.github.com>
2020-10-15 19:42:37 +0300

    add argo ci workflows. (#4)
    * add argo ci workflows

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
-import amazon_advertising_api.versions as aa_versions
 
 
 setup(
     name='amazon_advertising_api',
     packages=['amazon_advertising_api'],
-    version=aa_versions.versions['application_version'],
+    version='0.0.1',
     description='Unofficial Amazon Sponsored Products Python client library.',
     url='https://github.com/pepsico-ecommerce/amazon-advertising-api-python')
